295572eb5cc69dbdfbfb15b5faf516bf5447daba
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2021-01-21 08:40:07 +0000

    first commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Glowier
+
+<!-- Badges: Made with Sandstone; Supports 21w03a -->
+
+## Features
+
+- Room for expansion
---
+++ sandstone.config.ts
@@ -0,0 +1,10 @@
+import type { SandstoneConfig } from 'sandstone/core'
+
+export default {
+  name: 'glowier',
+  description: [ 'A ', { text: 'Sandstone', color: 'gold' }, ' data pack.' ],
+  formatVersion: 6,
+  namespace: 'glowier',
+  packUid: 'GD3yNkrq',
+  saveOptions: { path: '' }
+} as SandstoneConfig
